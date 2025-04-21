e8ebba0d09f7d2c9e10e7d54f018b7b1fb7c9302
AmmanSajid1 <ammansajid1@gmail.com>
2025-04-21 17:17:37 +0100

    Project structure and virtual env added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Amman Sajid",
+    author_email="ammansajid1@gmail.com",
+    packages=find_packages()
+)
